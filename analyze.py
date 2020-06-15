4a7ae491bb95b1f2e78b24948d496562fe3ba6e2
sachour <sachour@mit.edu>
2020-06-15 14:39:31 -0400

    analysis script correctly loads one of the entries

--- analyze.py
+++ analyze.py
@@ -6,10 +6,13 @@ import hwlib.hcdc.llenums as llenums
 import hwlib.hcdc.llcmd as llcmd
 import hwlib.hcdc.hcdcv2 as hcdclib
 
+def get_subarray(arr,inds):
+  return list(map(lambda i: arr[i], inds))
+
 def analyze(db,block,inst,out,cfg):
   def valid_data_point(dataset,method,idx):
     return dataset.meas_status[idx] == llenums.ProfileStatus.SUCCESS \
-      and dataset.method[idx] == method
+      and dataset.meas_method[idx] == method
 
   phys_block = llcmd.phys_block(db,block,inst,out,cfg)
   dataset = phys_block.dataset
@@ -18,7 +21,10 @@ def analyze(db,block,inst,out,cfg):
                                                   llenums.ProfileOpType.INPUT_OUTPUT, \
                                                   idx), range(0,dataset.size)))
 
-  print(indices)
+  meas = get_subarray(dataset.meas_mean,indices)
+  meas_stdev = get_subarray(dataset.meas_stdev,indices)
+  ref = get_subarray(dataset.output, indices)
+
 
 
 dev = hcdclib.get_device()
@@ -27,7 +33,15 @@ block = dev.get_block('mult')
 inst = devlib.Location([0,3,2,0])
 cfg = adplib.BlockConfig.make(block,inst)
 #cfg.modes = [['+','+','-','m']]
-cfg.modes = [['x','h','m']]
+cfg.modes = [block.modes.get(['x','h','m'])]
+# program hidden codes
+cfg.get('pmos').value = 0
+cfg.get('nmos').value = 0
+cfg.get('gain_cal').value = 0
+cfg.get('bias_in0').value = 0
+cfg.get('bias_in1').value = 0
+cfg.get('bias_out').value = 0
+
 out = block.outputs["z"]
 
 db = physdb.PhysicalDatabase('board6')
