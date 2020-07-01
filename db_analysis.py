13c97315e9365a7683588cdb8ea941db085a0524
Jack White <jackajwhite@gmail.com>
2020-07-01 20:10:35 +0100

    improved visualisation functionality including interpolation for incomplete datasets

--- db_analysis.py
+++ db_analysis.py
@@ -1,4 +1,5 @@
 from scipy.optimize import curve_fit
+from scipy.interpolate import griddata
 import numpy as np
 import hwlib.physdb as physdb
 import hwlib.hcdc.hcdcv2 as hcdclib
@@ -6,12 +7,13 @@ import hwlib.adp as adplib
 import hwlib.block as blocklib
 import matplotlib.pyplot as plt
 import math
+import itertools
 
 dev = hcdclib.get_device()
 db = physdb.PhysicalDatabase('board6')
 
 xcat = "bias_in0" #this can change
-ycat = "nmos" #this can change 
+ycat = "bias_in1" #this can change 
 zcat = "param_D" #this should be either "cost", "param_A", "param_D"
 
 data_list_of_dicts = []
@@ -42,9 +44,9 @@ isolate_rows = []
 
 #this is specific to the data being compared, will need to be changed if xcat,ycat change
 
-
-#FOR COMPARING PMOS, NMOS
 '''
+#FOR COMPARING PMOS, NMOS
+
 for row in data_list_of_dicts:
 	if (row["gain_cal"] == row["bias_in0"]) and \
 	(row["gain_cal"] == row["bias_in1"]) and \
@@ -52,6 +54,7 @@ for row in data_list_of_dicts:
 		isolate_rows.append(row)
 '''
 
+'''
 #FOR COMPARING NMOS, BIAS_IN0
 for row in data_list_of_dicts:
 	if (row["gain_cal"] == row["bias_in1"]) and \
@@ -59,6 +62,15 @@ for row in data_list_of_dicts:
 	(row["pmos"] == 3):
 		isolate_rows.append(row)
 
+'''
+
+
+#FOR COMPARING BIAS_IN0, BIAS_IN1
+for row in data_list_of_dicts:
+	if (row["gain_cal"] == row["bias_out"]) and \
+	(row["nmos"] == row["pmos"]) and \
+	(row["nmos"] == 3) and (row["gain_cal"] == 16):
+		isolate_rows.append(row)
 
 
 #print(isolate_rows)
@@ -84,18 +96,26 @@ THIS IS A HACK - to make an 8x8 heatmap matplotlib.imshow wants an array which i
 axes vary from 0->28 if you are plotting bias_something or gain_cal, so you need to divide by 4 to assemble the array
 '''
 
-data_to_plot = np.zeros((8,8))
+data_to_plot = np.zeros((max(ycat_list)+1,max(xcat_list)+1))
+data_to_plot[:] = np.NaN
 
+#print(data_to_plot)
+data_to_assemble = np.array(data_to_assemble)
 for datapoint in data_to_assemble:					
-	#xcoord_in_array = datapoint[0]					# if xcat is pmos, nmos
-	xcoord_in_array = math.floor(datapoint[0]/4)	# if xcat is bias_XXX or gain_cal 
-	ycoord_in_array = datapoint[1]					# if ycat is pmos, nmos
+	xcoord_in_array = math.floor(datapoint[0])					# if xcat is pmos, nmos
+	#xcoord_in_array = math.floor(datapoint[0]/4)	# if xcat is bias_XXX or gain_cal 
+	ycoord_in_array = math.floor(datapoint[1])					# if ycat is pmos, nmos
 	#ycoord_in_array = math.floor(datapoint[1]/4)	# if ycat is bias_XXX or gain_cal 
 	zcoord = datapoint[2]
-	data_to_plot[xcoord_in_array][ycoord_in_array] = zcoord
+	#print(ycoord_in_array)
+	#print(xcoord_in_array)
+	data_to_plot[ycoord_in_array,xcoord_in_array] = zcoord
 
-x_axis_list = sorted(set(xcat_list))
-y_axis_list = sorted(set(ycat_list))
+#x_axis_list = sorted(set(xcat_list))
+#y_axis_list = sorted(set(ycat_list))
+x_axis_list = np.arange(min(xcat_list),max(xcat_list))
+y_axis_list = np.arange(min(ycat_list),max(ycat_list)+1)
+
 
 fi, ax = plt.subplots()
 im = ax.imshow(data_to_plot)
@@ -107,11 +127,32 @@ ax.set_yticklabels(y_axis_list)
 
 ax.set_title(zcat)
 
-#print(data_to_plot)
-plt.imshow(data_to_plot, cmap='hot', interpolation='nearest')
+
+#INTERPOLATE
+points_to_interpolate = []
+for coordinate in itertools.product(x_axis_list,y_axis_list):
+	points_to_interpolate.append(coordinate)
+
+
+grid_x, grid_y = np.mgrid[0:len(x_axis_list),0:len(y_axis_list)]
+
+
+data_to_plot2 = griddata(data_to_assemble[:,:2], data_to_assemble[:,2], (grid_x, grid_y), method="cubic")
+
+
+constructed_data = np.zeros((len(y_axis_list),len(x_axis_list)))
+for x in range(len(data_to_plot2)):
+	for y in range(len(data_to_plot2[0])):
+		constructed_data[y,x] = data_to_plot2[x,y]
+
+
+im = plt.imshow(constructed_data, cmap='hot', interpolation='nearest')
+#im = plt.imshow(data_to_plot, cmap='hot', interpolation='nearest')
 plt.gca().invert_yaxis()		#THIS MIGHT BE CAUSING PROBLEMS
 plt.xlabel(xcat)
 plt.ylabel(ycat)
+cbar = ax.figure.colorbar(im, ax=ax)
+cbar.ax.set_ylabel("value",rotation=-90,va="bottom")
 
 plt.show()
 
