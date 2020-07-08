c366b2063c4348b5449217ae6a9d3d944da359e0
sachour <sachour@mit.edu>
2020-07-08 11:12:33 -0400

    fixed

--- compiler/lgraph_pass/route.py
+++ compiler/lgraph_pass/route.py
@@ -1,4 +1,8 @@
 
 
 def route(board,circ):
+  # assign each block to a chip
+  # assign each block to a tile, given chip assignments
+  # assign each block to a slice, given tile assignments
+  # assign each block to a index, given slice assignments
   raise NotImplementedError
