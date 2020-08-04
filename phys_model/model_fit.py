c1bd226c1f70f7c51e52442775beecb4ed8c52e9
Jack White <jackajwhite@gmail.com>
2020-08-04 22:05:17 +0100

    fixed debug print len() statements

--- phys_model/model_fit.py
+++ phys_model/model_fit.py
@@ -133,6 +133,7 @@ def fit_model(variables,expr,data):
   repl = {}
   dataset = [None]*n_inputs
   for idx,bound_var in enumerate(inputs.keys()):
+    print("inputs: ", inputs, "\n")
     print("len(inputs[pmos]:", len(inputs['pmos']), "\n")
     print("len(meas_output):", len(meas_output), "\n")
     assert(len(inputs[bound_var]) == len(meas_output))
