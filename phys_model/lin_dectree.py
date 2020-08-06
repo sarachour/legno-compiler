ab0b5be5d88f62d377a2de4313a36b7e5c46fe90
Jack White <jackajwhite@gmail.com>
2020-08-06 20:17:27 +0100

    debugging bounds

--- phys_model/lin_dectree.py
+++ phys_model/lin_dectree.py
@@ -94,6 +94,7 @@ class RegressionLeafNode:
 
   def find_minimum(self,bounds):
     hidden_vars = self.expr.vars()
-    optimal_codes = fitlib.minimize_model(hidden_vars, self.expr, {}, bounds) 
+    optimal_codes = fitlib.minimize_model(hidden_vars, self.expr, {}, bounds)
+    print("minimizing model with bounds: ", bounds) 
     return optimal_codes['objective_val'], optimal_codes['values']
 
