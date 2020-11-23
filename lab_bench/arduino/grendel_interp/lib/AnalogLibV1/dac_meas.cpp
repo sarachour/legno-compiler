cc63206591c9ad07e9aff48be0057bb164310ef2
Sara Achour <sachour@mit.edu>
2020-11-23 14:48:13 -0500

    fixed the issue i think

--- lab_bench/arduino/grendel_interp/lib/AnalogLibV1/dac_meas.cpp
+++ lab_bench/arduino/grendel_interp/lib/AnalogLibV1/dac_meas.cpp
@@ -69,17 +69,15 @@ profile_t Fabric::Chip::Tile::Slice::Dac::measureConstVal(profile_spec_t spec)
 
   dac_to_tile.setConn();
 	tile_to_chip.setConn();
-  this->m_state = spec.state.dac;
   float target = this->computeOutput(this->m_state);
   float mean,variance;
   mean = this->fastMeasureValue(variance);
 #ifdef DEBUG_DAC_PROF
-  sprintf(FMTBUF,"prof-dac code=%d targ=%f mean=%f variance=%f\n",
-          this->m_state.const_code, target,mean,variance);
+  sprintf(FMTBUF,"prof-dac in=%f code=%d targ=%f mean=%f variance=%f\n",
+          spec.inputs[in0Id], \
+          this->m_state.const_code,target,mean,variance);
   print_info(FMTBUF);
 #endif
-  const int mode = 0;
-  const float in1 = 0.0;
   profile_t result = prof::make_profile(spec,
                                         mean,
                                         sqrt(variance));
