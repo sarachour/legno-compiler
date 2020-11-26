c6e203140690314b08a35417892b1fa727c2ca80
Jessica Ray <jray@csail.mit.edu>
2020-11-25 20:15:17 -0500

    modified dma

--- lab_bench/arduino/grendel_interp/lib/AnalogLibV1/dma.cpp
+++ lab_bench/arduino/grendel_interp/lib/AnalogLibV1/dma.cpp
@@ -36,21 +36,36 @@ namespace dma {
 
   uint32_t find_prescalar(float runtime, uint32_t bufsiz){
     uint32_t presc = 0;
+    uint32_t clk;
     float n_samples;
     sprintf(FMTBUF,"runtime=%f buffer-size=%d\n",runtime,bufsiz);
     print_info(FMTBUF);
     do{
       ADC->ADC_MR &= ~ADC_MR_PRESCAL_Msk;
       ADC->ADC_MR |= ADC_MR_PRESCAL(presc);// set prescaler to fastest
-      uint32_t clk = adc_get_actual_adc_clock(ADC,SystemCoreClock);
+      clk = adc_get_actual_adc_clock(ADC,SystemCoreClock);
       n_samples = runtime*clk;
       sprintf(FMTBUF,"  prescalar=%d clk=%d khz samps=%f\n", presc, clk/1000,n_samples);
       print_info(FMTBUF);
       presc += 1;
-    } while(n_samples > bufsiz);
+    } while(n_samples > bufsiz || clk > ADC_FREQ_MAX);
     return presc;
   }
-
+  void track_adc_0(){
+    ADC->ADC_COR = ADC_COR_DIFF1 | ADC_COR_DIFF0 | ADC_COR_OFF0 | ADC_COR_OFF1;
+    ADC->ADC_CHER |= ADC_CHER_CH0 | ADC_CHER_CH1;
+    // enable interrupt
+    uint32_t interrupts = ADC_IER_EOC0;
+    adc_disable_interrupt(ADC, ~interrupts);
+    adc_enable_interrupt(ADC, interrupts);
+  }
+  void track_adc_6(){
+    ADC->ADC_COR = ADC_COR_DIFF7 | ADC_COR_DIFF6 | ADC_COR_OFF6 | ADC_COR_OFF7;
+    ADC->ADC_CHER |= ADC_CHER_CH6 | ADC_CHER_CH7;
+    // enable interrupt
+    uint32_t interrupts = ADC_IER_EOC6;
+    adc_enable_interrupt(ADC, interrupts);
+  }
   void setup(dma_info_t& info,
              float runtime,
              uint16_t * buf, uint32_t siz){
@@ -59,45 +74,36 @@ namespace dma {
     print_info("configuring DMA\n");
     store_dma_state(info);
     summarize_dma_state(info);
-
+    adc_stop(ADC);
     print_info("  -> reset data buffer\n");
     memset(buf,0,siz*sizeof(uint16_t));
-
     print_info("  -> configure adc sampler\n");
     ADC->ADC_MR |= ADC_MR_FREERUN_ON; // set free running mode on ADC
-    //ADC->ADC_MR &= 0xFFFF00FF; // set prescaler to fastest
     ADC->ADC_MR &= ~ADC_MR_PRESCAL_Msk;
     int presc = find_prescalar(runtime,siz);
     ADC->ADC_MR |= ADC_MR_PRESCAL(presc);// set prescaler to fastest
+    if(ADC->ADC_ISR & ADC_ISR_ENDRX){
+    	print_info("post clock... buffer is populated?\n");
+    }
+    adc_enable_anch(ADC);
+    track_adc_6();    
 
-    adc_enable_channel_differential_input(ADC, ADC_CHANNEL_0);
-    //ADC->ADC_COR = 0x10000;  // enable differential ADC for all channels
-    //ADC->ADC_COR = 0x00000; // single ended mode for all channels
-    adc_enable_channel(ADC,ADC_CHANNEL_0);
-    adc_enable_channel(ADC,ADC_CHANNEL_2);
-    adc_enable_channel(ADC,ADC_CHANNEL_4);
-    adc_enable_channel(ADC,ADC_CHANNEL_6);
-
-    // enable interrupt
-    uint32_t interrupts = ADC_IER_DRDY | ADC_IER_EOC0 | ADC_IER_EOC2
-      | ADC_IER_EOC4 | ADC_IER_EOC6;
-    adc_enable_interrupt(ADC, interrupts);
-    store_dma_state(new_info);
-    summarize_dma_state(new_info);
- 
     print_info("  -> instantiated buffers\n");
     // configure the buffer to fill
     ADC->ADC_RPR = (uint32_t) buf;
     ADC->ADC_RCR = siz;
-    ADC->ADC_PTCR = ADC_PTCR_RXTEN;
-    //
-    while ((adc_get_status(ADC) & ADC_ISR_DRDY) == 0);
 
+    print_info("  -> checking adc config\n");
+    sprintf(FMTBUF, "min adc freq: %d\n", ADC_FREQ_MIN);
+    print_info(FMTBUF);
+    sprintf(FMTBUF, "max adc freq: %d\n", ADC_FREQ_MAX);
+    print_info(FMTBUF);
     print_info("...done\n");
   }
 
   void run(){
-    ADC->ADC_CR |= ADC_CR_START;
+    ADC->ADC_PTCR = ADC_PTCR_RXTEN;
+    adc_start(ADC);
     while(!(ADC->ADC_ISR & ADC_ISR_ENDRX)){
       delay(1);
     }
